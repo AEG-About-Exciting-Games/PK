0109976cb193094b04ebc4ab627c274c11995870
AMinSC <cdtjdcjf@gmail.com>
2024-09-15 14:25:42 +0900

    타입힌트 추가

--- popcornKiller/pk_apis/movies_api.py
+++ popcornKiller/pk_apis/movies_api.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from typing import Optional, Dict
 import os
 
 from pk_utils.api_utils import fetch_api_data
@@ -9,7 +10,7 @@ load_dotenv()
 API_KEY = os.getenv("MOVIE_API_KEY")
 
 
-def get_people_cd(people_nm):
+def get_people_cd(people_nm: str) -> Optional[str]:
     url = (f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?'
             f'key={API_KEY}&peopleNm={people_nm}')
     people = fetch_api_data(url)
@@ -19,13 +20,13 @@ def get_people_cd(people_nm):
     return None
 
 
-def get_actor_info(people_cd):
+def get_actor_info(people_cd) -> Optional[Dict[str, str]]:
     url = (f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleInfo.json?'
             f'key={API_KEY}&peopleCd={people_cd}')
     return fetch_api_data(url)
 
 
-def get_daily_movie_chart():
+def get_daily_movie_chart() -> Optional[Dict[str, str]]:
     date = (datetime.now().date() - timedelta(days=1)).strftime('%Y%m%d')
     url = (f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?'
             f'key={API_KEY}&targetDt={date}')
@@ -33,13 +34,13 @@ def get_daily_movie_chart():
 
 
 
-def get_movie_detail(movie_cd):
+def get_movie_detail(movie_cd) -> Optional[Dict[str, str]]:
     url = (f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?'
             f'key={API_KEY}&movieCd={movie_cd}')
     return fetch_api_data(url)
 
 
-def get_movie_search_list(movie_name):
+def get_movie_search_list(movie_name) -> Optional[str]:
     url = (f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?'
             f'key={API_KEY}&movieNm={movie_name}')
     return fetch_api_data(url)
--- popcornKiller/pk_utils/general_utils.py
+++ popcornKiller/pk_utils/general_utils.py
@@ -1,5 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def get_error_response(request, message):
+def get_error_response(request: HttpRequest, message: str) -> HttpResponse:
     return render(request, 'error.html', {'error': message})
